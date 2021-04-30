edb69f9a66514e2148182f1765085d3d4b928466
Rajvi Jasani <rajvijasani1@gmail.com>
2021-04-30 21:50:54 +0530

    Modified Frontend

--- script_login_signup.js
+++ script_login_signup.js
@@ -7,27 +7,27 @@ addSignIn();
 signin.addEventListener('click', () => {
     arrow.classList.add('point-signin');
     arrow.classList.remove('point-signup', 'point-forgot');
-    signin.style.color = '#d2e603';
-    signup.style.color = '#3e978b';
-    forgot.style.color = '#3e978b';
+    signin.style.color = '#fff';
+    signup.style.color = '#0a6155';
+    forgot.style.color = '#0a6155';
     removeAllElements(divinput);
     addSignIn();
 });
 signup.addEventListener('click', () => {
     arrow.classList.add('point-signup');
     arrow.classList.remove('point-signin', 'point-forgot');
-    signin.style.color = '#3e978b';
-    signup.style.color = '#d2e603';
-    forgot.style.color = '#3e978b';
+    signin.style.color = '#0a6155';
+    signup.style.color = '#fff';
+    forgot.style.color = '#0a6155';
     removeAllElements(divinput);
     addSignUp();
 });
 forgot.addEventListener('click', () => {
     arrow.classList.add('point-forgot');
     arrow.classList.remove('point-signin', 'point-signup');
-    signin.style.color = '#3e978b';
-    signup.style.color = '#3e978b';
-    forgot.style.color = '#d2e603';
+    signin.style.color = '#0a6155';
+    signup.style.color = '#0a6155';
+    forgot.style.color = '#fff';
     removeAllElements(divinput);
     addForgot();
 });
@@ -54,6 +54,14 @@ function addSignIn() {
 function addSignUp() {
     divinput.action = '';
     divinput.action = '/signup';
+    const name = document.createElement('input');
+    name.type = "text";
+    name.placeholder = "Full Name";
+    divinput.appendChild(name);
+    const number = document.createElement('input');
+    number.type = "text";
+    number.placeholder = "Mobile Number";
+    divinput.appendChild(number);
     const email = document.createElement('input');
     email.type = "email";
     email.placeholder = "Email";
@@ -64,7 +72,7 @@ function addSignUp() {
     divinput.appendChild(password);
     const rpassword = document.createElement('input');
     rpassword.type = "password";
-    rpassword.placeholder = "Repeat password";
+    rpassword.placeholder = "Repeat Password";
     divinput.appendChild(rpassword);
     const button = document.createElement('button');
     button.textContent = "Sign Up";
--- script_main.js
+++ script_main.js
@@ -1,12 +1,27 @@
 const bubble = document.querySelector('.bubble');
 const bar = document.querySelector('.bar');
-const list = document.querySelectorAll('#icons');
+const list = document.querySelectorAll('.icons');
 let coords = list[0].getBoundingClientRect();
 setBounds(coords);
+const container = document.querySelector('.container');
+addAnalytics();
 for (let i of list) {
     i.addEventListener('click', () => {
         let coords = i.getBoundingClientRect();
         setBounds(coords);
+        if (i.id === "analytics") {
+            removeAllElements(container);
+            addAnalytics();
+        }
+        else if (i.id === "photo") {
+            removeAllElements(container);
+            addPhoto();
+        }
+        else {
+            removeAllElements(container);
+            addProfile();
+        }
+
     });
 }
 function setBounds(coords) {
@@ -18,4 +33,66 @@ function setBounds(coords) {
     bar.style.top = `${coords.y}px`;
     bar.style.width = `3px`;
     bar.style.height = `${coords.height}px`;
+}
+function removeAllElements(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+function addAnalytics() {
+    const graph = document.createElement('div');
+    graph.classList.add('graph');
+    graph.innerHTML = "Graph will be displayed here";
+    container.appendChild(graph);
+    const stats = document.createElement('div');
+    stats.classList.add('stats');
+    stats.innerHTML = "Statistics will be shown here";
+    container.appendChild(stats);
+}
+function addPhoto() {
+    const upload = document.createElement('div');
+    upload.classList.add('photo');
+    const uploadlb = document.createElement('label');
+    uploadlb.innerHTML = "Click here to upload an existing photo";
+    upload.appendChild(uploadlb);
+    const uploadbtn = document.createElement('button');
+    uploadbtn.classList.add('photoBtn');
+    uploadbtn.innerHTML = "Upload";
+    upload.appendChild(uploadbtn);
+    container.appendChild(upload);
+
+    const capture = document.createElement('div');
+    capture.classList.add('photo');
+    const capturelb = document.createElement('label');
+    capturelb.innerHTML = "Click here to capture<br>a new photo";
+    capture.appendChild(capturelb);
+    const capturebtn = document.createElement('button');
+    capturebtn.classList.add('photoBtn');
+    capturebtn.innerHTML = "Capture";
+    capture.appendChild(capturebtn);
+    container.appendChild(capture);
+
+}
+function addProfile() {
+    const profile = document.createElement('div');
+    profile.classList.add('profile');
+    const img = document.createElement('img');
+    img.src = "resources/profile_w.png";
+    img.id = "profile";
+    img.classList.add("icons");
+    img.alt = "Profile";
+    profile.appendChild(img);
+    const name = document.createElement('div');
+    name.classList.add('details');
+    name.innerHTML = "Full Name";
+    profile.appendChild(name);
+    const number = document.createElement('div');
+    number.classList.add('details');
+    number.innerHTML = "Mobile Number";
+    profile.appendChild(number);
+    const email = document.createElement('div');
+    email.classList.add('details');
+    email.innerHTML = "Email";
+    profile.appendChild(email);
+    container.appendChild(profile);
 }
